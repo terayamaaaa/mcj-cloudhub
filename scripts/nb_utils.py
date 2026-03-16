130e160c285ada9d54f33a200222658a8bde7807
terayamaaaa <terayama@ascade.co.jp>
2026-03-17 00:34:59 +0900

    fix: Fix invalid output when progress view out to cell output.
    Because the html converted by nbconvert include lab styles and that effect to page, so specify `template_name=classic` when nbconvert.

--- scripts/nb_utils.py
+++ scripts/nb_utils.py
@@ -39,8 +39,7 @@ def generate_password(length=32):
 
         if (any(c.islower() for c in password)
                 and any(c.isupper() for c in password)
-                and any(c.isdigit() for c in password)
-                and any(c in "!#$%&*+-=?@^_" for c in password)):
+                and any(c.isdigit() for c in password)):
             return password
 
 
